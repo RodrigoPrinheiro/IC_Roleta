2ecb1db8d79206d52bb8e554eee3d07dcbae9ba5
Thomas <tfranque@gmail.com>
2018-11-10 17:59:37 +0000

    colorindentifier

--- arroz_funcoes.c
+++ arroz_funcoes.c
@@ -87,3 +87,45 @@ int option_select (){
     return bet;
 }
 */
+
+
+
+
+
+
+int color_check(int a, int b){
+  int red[18]={1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
+  int black[18]={2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
+
+  int controller = 0;
+  int x=0;
+  int color = b;
+
+  if (color == 1){ // if player chooses red = 1
+    for (x = 1; x < 18; x++) {
+      if(a == red[x]){
+        controller = 1;
+        if (controller == 1){
+          printf("You win!");
+        }
+      }
+    }
+    if (controller == 0){
+      printf("you lose.");
+    }
+  }
+
+  if (color == 2){ // if player chooses black = 2
+    for (x = 1; x < 18; x++) {
+      if(a == black[x]){
+        controller = 1;
+        if (controller == 1){
+          printf("You win!");
+        }
+      }
+    }
+    if (controller == 0){
+      printf("you lose.");
+    }
+  }
+}
